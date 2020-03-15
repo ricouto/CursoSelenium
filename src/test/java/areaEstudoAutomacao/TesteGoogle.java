3de77d0237e6ce16bb4c726128ddb58cf0dc2775
Ricardo <ricardocaputo@gmail.com>
2020-03-15 13:31:50 -0300

    ajustado o codigo para Junit

--- src/test/java/areaEstudoAutomacao/TesteGoogle.java
+++ src/test/java/areaEstudoAutomacao/TesteGoogle.java
@@ -1,11 +1,12 @@
 package areaEstudoAutomacao;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 //import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -13,37 +14,56 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TesteGoogle {
 
-	@Test
-	public void teste() throws InterruptedException {
+	static WebDriver driver;
+
+	@BeforeClass
+	public static void iniciaNavegador() throws InterruptedException {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
-		 
-		System.setProperty("webdriver.chrome.driver",
-		"C:\\Users\\HiTECH-PC\\workspace\\CursoSelenium\\src\\test\\resources\\chromedriver.exe");
+
+		System.setProperty(
+				"webdriver.chrome.driver",
+				"C:\\Users\\HiTECH-PC\\workspace\\CursoSelenium\\src\\test\\resources\\chromedriver.exe");
 		// WebDriver driver = new FirefoxDriver();
-		
-		WebDriver driver = new ChromeDriver(options);
-				
+
+		driver = new ChromeDriver(options);
+
 		// WebDriver driver = new InternetExplorerDriver();
 
 		driver.get("http://www.google.com");
+	}
+
+	@AfterClass
+	public static void finalizaNavegador() throws InterruptedException {
+		if (driver != null)
+			driver.quit();
+	}
+
+	@Test
+	public void teste() throws InterruptedException {
+
 		Assert.assertEquals("Google", driver.getTitle());
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("informatica");
-				
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys(
+				"informatica");
 
-		String returnText = driver.findElement(By.xpath("//div[@id='result-stats']")).getText().substring(0, 15);
-		System.out.println("\nApresentou o resultado na tela >> " + returnText +"\n");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys(
+				Keys.ENTER);
+
+		String returnText = driver
+				.findElement(By.xpath("//div[@id='result-stats']")).getText()
+				.substring(0, 15);
+		System.out.println("\nApresentou o resultado na tela >> " + returnText
+				+ "\n");
 		Thread.sleep(5000);
 		if (returnText.equalsIgnoreCase("Aproximadamente")) {
 			Assert.assertTrue(true);
-			System.out.println(driver.findElement(By.xpath("//div[@id='result-stats']")).getText());
+			System.out.println(driver.findElement(
+					By.xpath("//div[@id='result-stats']")).getText());
 		} else {
 			System.out.println("Favor verificar o texto inserido no Google");
 			Assert.fail();
 		}
-		driver.quit();
 	}
 }
