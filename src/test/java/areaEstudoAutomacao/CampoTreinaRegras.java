1f89119ebd848dd51464dc80e7cdeeafebeb78ad
Ricardo <ricardocaputo@gmail.com>
2020-04-02 15:52:43 -0300

    subindo 02/04

--- src/test/java/areaEstudoAutomacao/CampoTreinaRegras.java
+++ src/test/java/areaEstudoAutomacao/CampoTreinaRegras.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
-import org.testng.Assert;
 
 @RunWith(Parameterized.class)
 public class CampoTreinaRegras extends ConexaoDrive {
--- src/test/java/areaEstudoAutomacao/ExecuteTest.java
+++ src/test/java/areaEstudoAutomacao/ExecuteTest.java
@@ -2,41 +2,17 @@ package areaEstudoAutomacao;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-/*
-import org.junit.runner.JUnitCore;		
-import org.junit.runner.Result;		
-import org.junit.runner.notification.Failure;
-*/
 
 	@RunWith(Suite.class)				
 	@Suite.SuiteClasses({				
 	  TesteGoogle.class,
 	  SiteAleatorio.class,
-	  CampoTreinaRegras.class,
 	  SiteEstudo.class,
+	  CampoTreinaRegras.class,
 	  TestAPI.class,
 	  SiteAleatorioCopy.class
 	})	
-		
 
 	public class ExecuteTest {				
 						
 	}
-
-
-/*
-public class ExecuteTest {
-	public static void main(String[] args) {
-		Result result = JUnitCore.runClasses(
-				TesteGoogle.class,
-				SiteAleatorio.class,
-				TestAPI.class
-				);
-		for (Failure failure : result.getFailures()) {
-			System.out.println(failure.toString());
-		}
-		System.out.println("Result == " + result.wasSuccessful());
-	}
-	
-}	
-	*/
